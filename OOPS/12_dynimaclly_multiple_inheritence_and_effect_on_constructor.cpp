c892099d5d20b63679f069edef7b8121be63aafc
skysumitkumar <152373592+skysumitkumar@users.noreply.github.com>
2024-11-12 18:08:44 +0530

    Update 12_dynimaclly_multiple_inheritence_and_effect_on_constructor.cpp

--- OOPS/12_dynimaclly_multiple_inheritence_and_effect_on_constructor.cpp
+++ OOPS/12_dynimaclly_multiple_inheritence_and_effect_on_constructor.cpp
@@ -30,27 +30,29 @@ class cat:public Animal
 int main()
 {   
     // //upstreaming
-    // //firstly we use cat class and cat obj
+    // //firstly we use cat class reference and cat class obj
     // //when we make obj of cat than firstly animal constructor than cat constructor is called
     // dynamically allocate memory for an object of type cat and initialize a pointer to this object.
     // cat * c=new cat;
     
     
     // //upstreaming
-    // //here we use Animal class and Animal obj
+    // //here we use Animal class reference and Animal class obj
     // //when we make animal obj than it call only animal constructor
     // dynamically allocate memory for an object of type Animal and initialize a pointer to this object.
     // Animal * c=new Animal;
     
 
     // //upstreaming
-    // //here we use Animal class and cat obj
+    // //here we use Animal class reference and cat class obj
+    // It call members and methods of the reference class and we change object class variable using getter and setter we help of virtual keyword in base class
     // //when we make the Animal class obj than it call firstly Animal constructor than call cat constructor
     // Animal * c=new cat;
     
 
     // //downstreaming
-    // //here we use cat class and Animal class as object
+    // It call members and methods of the reference class and we change object class variable with class name and scope resolution operator because here we inherited the reference class
+    // //here we use cat class reference and Animal class as object
     // //it give error when we use previous method for downstreaming
     // //cat * c=new Animal;
     // //for downstreaming we use this
@@ -58,7 +60,4 @@ int main()
     // cat* c=(cat*) new Animal;
     // Animal* a = new cat(); // Correct upcasting, cat is a type of Animal it calls animal constructor
     // cat* c = dynamic_cast<cat*>(a); // Safe downcasting it calls cat constructor
-    
-
-
 }
