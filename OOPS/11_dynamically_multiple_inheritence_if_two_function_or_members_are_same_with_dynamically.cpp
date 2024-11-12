6de3c9f755d0852e97fcbaafaabec96f76becc5f
skysumitkumar <152373592+skysumitkumar@users.noreply.github.com>
2024-11-12 18:03:12 +0530

    Update 11_dynamically_multiple_inheritence_if_two_function_or_members_are_same_with_dynamically.cpp

--- OOPS/11_dynamically_multiple_inheritence_if_two_function_or_members_are_same_with_dynamically.cpp
+++ OOPS/11_dynamically_multiple_inheritence_if_two_function_or_members_are_same_with_dynamically.cpp
@@ -59,9 +59,11 @@ int main()
     // // upstreaming
     // //here we use Animal class reference and cat class obj we firstly create cat object and then assigns it to an Animal pointer(reference)
     // Animal * c=new cat;
+    // It call reference class members and methods
     // c->color="black";
     // //when we are assigning value to cat class it give error because here we make Animal class obj and Animal class not inherited cat class
     // //c->cat::color="brown";
+    // If we want to change the member variable value of the cat class we have to use getter and setter
     // c->printcolor();
     // //when we want to call  print color of cat class than we have to use virtual keyword in Animal class to enable polymorphism than it go to print color of the cat class
     // //c->printcolor();
@@ -89,6 +91,7 @@ int main()
     {
     // Successfully downcasted to cat
     c->cat::color="brown";
+    // It change the color of the Animal class because cat class inherit Animal class
     c->Animal::color="black";
     c->color="pink";//it change the cat color
     c->printcolor();//it print the cat color
@@ -102,3 +105,202 @@ int main()
     }
 
 }
+//upcasting
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// class Animal
+// {
+//     public:
+//     int height;
+//     int size;
+//     int weight;
+//     string color="pink";
+//     Animal()
+//     {
+//         cout<<"I Am In The Animal Constructor"<<endl;
+//     }
+//     void printcolor()
+//     {
+//         cout<<"i am in Animal class"<<endl;
+//         cout<<color<<endl;
+//     }
+//    virtual ~Animal(){}
+// };
+// class cat:public Animal
+// {
+//     public:
+//     int height;
+//     string color="BLUE";
+//     cat()
+//     {
+//         cout<<"I Am In The Cat Constructor"<<endl;
+//     }
+//     void printcolor()
+//     {
+//         cout<<"i am in cat class"<<endl;
+//         cout<<color<<endl;
+//     }
+// };
+// int main()
+// {   
+//     Animal * c=new cat;
+// It call reference class member and methods
+//     cout<<c->color<<endl; //pink
+//     c->printcolor();      //pink
+// }
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// class Animal
+// {
+//     public:
+//     int height;
+//     int size;
+//     int weight;
+//     string color="pink";
+//     Animal()
+//     {
+//         cout<<"I Am In The Animal Constructor"<<endl;
+//     }
+//     virtual void printcolor()
+//     {
+//         cout<<"i am in Animal class"<<endl;
+//         cout<<color<<endl;
+//     }
+//    virtual ~Animal(){}
+// };
+// class cat:public Animal
+// {
+//     public:
+//     int height;
+//     string color="BLUE";
+//     cat()
+//     {
+//         cout<<"I Am In The Cat Constructor"<<endl;
+//     }
+//     void printcolor()
+//     {
+//         cout<<"i am in cat class"<<endl;
+//         cout<<color<<endl;
+//     }
+// };
+// int main()
+// {   
+//     Animal * c=new cat;
+//     cout<<c->color<<endl; //pink 
+// When we use virtual it call object class functions 
+//     c->printcolor();      //BLUE
+// }
+
+// //downcasting
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// class Animal
+// {
+//     public:
+//     int height;
+//     int size=50;
+//     int weight;
+//     string color="pink";
+//     Animal()
+//     {
+//         cout<<"I Am In The Animal Constructor"<<endl;
+//     }
+//     void printcolor()
+//     {
+//         cout<<"i am in Animal class"<<endl;
+//         cout<<color<<endl;
+//     }
+//    virtual ~Animal(){}
+// };
+// class cat:public Animal
+// {
+//     public:
+//     int height;
+//     int size=60;
+//     string color="blue";
+//     cat()
+//     {
+//         cout<<"I Am In The Cat Constructor"<<endl;
+//     }
+//     void printcolor()
+//     {
+//         cout<<"i am in cat class"<<endl;
+//         cout<<color<<endl;
+//     }
+// };
+// int main()
+// {  
+//     Animal* a = new cat(); // Correct upcasting, cat is a type of Animal it calls animal constructor
+//     cat* c = dynamic_cast<cat*>(a); // Safe downcasting it calls cat constructor
+//     if(c!=nullptr) 
+//     {
+//         //It call reference class members and methods
+//         cout<<c->color<<endl;
+//         c->printcolor();
+//     delete c;  // Don't forget to delete the dynamically allocated object
+//     } 
+//     else 
+//     {
+//     // Failed to downcast, handle the error
+//     delete c;  // Delete the object since it's not a cat
+//     }
+// }
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// class Animal
+// {
+//     public:
+//     int height;
+//     int size=50;
+//     int weight;
+//     string color="pink";
+//     Animal()
+//     {
+//         cout<<"I Am In The Animal Constructor"<<endl;
+//     }
+//     virtual void printcolor()
+//     {
+//         cout<<"i am in Animal class"<<endl;
+//         cout<<color<<endl;
+//     }
+//    virtual ~Animal(){}
+// };
+// class cat:public Animal
+// {
+//     public:
+//     int height;
+//     int size=60;
+//     string color="blue";
+//     cat()
+//     {
+//         cout<<"I Am In The Cat Constructor"<<endl;
+//     }
+//     void printcolor()
+//     {
+//         cout<<"i am in cat class"<<endl;
+//         cout<<color<<endl;
+//     }
+// };
+// int main()
+// {  
+//     Animal* a = new cat(); // Correct upcasting, cat is a type of Animal it calls animal constructor
+//     cat* c = dynamic_cast<cat*>(a); // Safe downcasting it calls cat constructor
+//     if(c!=nullptr) 
+//     {
+//         //In case of virtual in any class it call reference class member and methods
+//         cout<<c->color<<endl;
+//         c->printcolor();
+//     delete c;  // Don't forget to delete the dynamically allocated object
+//     } 
+//     else 
+//     {
+//     // Failed to downcast, handle the error
+//     delete c;  // Delete the object since it's not a cat
+//     }
+// }
